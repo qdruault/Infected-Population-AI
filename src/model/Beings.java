90c8c0f3bf05c0cd2b0fec78a07eda5a9975b39f
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2017-05-30 18:54:44 +0200

    Fix addAgentsHuman

--- src/model/Beings.java
+++ src/model/Beings.java
@@ -9,6 +9,9 @@ import sim.util.Int2D;
 
 import java.util.Random;
 
+import model.Human.Condition;
+import model.Human.Gender;
+
 public class Beings extends SimState {
 
 
@@ -20,66 +23,72 @@ public class Beings extends SimState {
 	public void start() {
 		System.out.println("Simulation started");
 		super.start();
-	    yard.clear();
-	    addAgentsHuman();
-	    addAgentsFood();
-  }
+		yard.clear();
+		addAgentsHuman();
+		addAgentsFood();
+	}
 
-  /**
-   * Génère des Humains sur la carte.
-   */
-  public void addAgentsHuman(){
-	  for(int  i  =  0;  i  <  Constants.NUM_HUMANS;  i++) {
-		  //Human  a  =  new Human(random.nextInt(Constants.MAX_IMMUNITY), random.nextInt(31) + 20, random.nextInt(Constants.MAX_FERTILITY), random.nextInt(2), random.nextFloat());
-		  // Refaire avec les bons paramètres et des entiers.
-		  Human  a = new Human();
-		  Int2D location = getFreeLocation();
-		  yard.setObjectLocation(a, location.x, location.y);
-		  a.setX(location.x);
-		  a.setY(location.y);
-		  schedule.scheduleRepeating(a);
-	  }
-  }
+	/**
+	 * Génère des Humains sur la carte.
+	 */
+	public void addAgentsHuman(){
+		for(int  i  =  0;  i  <  Constants.NUM_HUMANS;  i++) {
+			Gender gender;
+			if (i % 2 == 0) {
+				gender = Gender.FEMALE;
+			} else {
+				gender = Gender.MALE;
+			}
+			int immunity = random.nextInt(Constants.MAX_IMMUNITY);
+			int fertility = random.nextInt(Constants.MAX_FERTILITY);
+			Human  a = new Human(immunity, fertility, gender, Condition.FINE);
+			Int2D location = getFreeLocation();
+			yard.setObjectLocation(a, location.x, location.y);
+			a.setX(location.x);
+			a.setY(location.y);
+			schedule.scheduleRepeating(a);
+		}
+	}
 
 
-  /**
-   * Génère de la nourriture sur la carte.
-   */
-  public void addAgentsFood(){
-	  for(int  i  =  0;  i  <  Constants.NUM_FOODS;  i++) {
-		  Food  a  =  new Food(random.nextInt(Constants.MAX_QUANTITY), random.nextInt(Constants.MAX_NUTRIOTINAL_PROVISION));
-		  Int2D location = getFreeLocation();
-		  yard.setObjectLocation(a, location.x, location.y);
-		  a.setX(location.x);
-		  a.setY(location.y);
-		  schedule.scheduleRepeating(a);
-	  }
-  }
+	/**
+	 * Génère de la nourriture sur la carte.
+	 */
+	public void addAgentsFood(){
+		for(int  i  =  0;  i  <  Constants.NUM_FOODS;  i++) {
+			Food  a  =  new Food(random.nextInt(Constants.MAX_QUANTITY), random.nextInt(Constants.MAX_NUTRIOTINAL_PROVISION));
+			Int2D location = getFreeLocation();
+			yard.setObjectLocation(a, location.x, location.y);
+			a.setX(location.x);
+			a.setY(location.y);
+			schedule.scheduleRepeating(a);
+		}
+	}
 
-  /**
-   * Indique si une case est libre.
-   * @param x
-   * @param y
-   * @return
-   */
-  public boolean free(int x,int y) {
-	 int xx = yard.stx(x);
-	 int yy = yard.sty(y);
-	 return yard.getObjectsAtLocation(xx,yy) == null;
-  }
-  
-  /**
-   * Trouve un endroit libre sur la carte.
-   * @return
-   */
-  private Int2D getFreeLocation() {
-	  Int2D location = new Int2D(random.nextInt(yard.getWidth()),
-	           random.nextInt(yard.getHeight()) );
-	  Object ag;
-	  while ((ag = yard.getObjectsAtLocation(location.x,location.y)) != null) {
-	   	  location = new Int2D(random.nextInt(yard.getWidth()),
-	   	           random.nextInt(yard.getHeight()) );
-	  }
-	  return location;
-  }
+	/**
+	 * Indique si une case est libre.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean free(int x,int y) {
+		int xx = yard.stx(x);
+		int yy = yard.sty(y);
+		return yard.getObjectsAtLocation(xx,yy) == null;
+	}
+
+	/**
+	 * Trouve un endroit libre sur la carte.
+	 * @return
+	 */
+	private Int2D getFreeLocation() {
+		Int2D location = new Int2D(random.nextInt(yard.getWidth()),
+				random.nextInt(yard.getHeight()) );
+		Object ag;
+		while ((ag = yard.getObjectsAtLocation(location.x,location.y)) != null) {
+			location = new Int2D(random.nextInt(yard.getWidth()),
+					random.nextInt(yard.getHeight()) );
+		}
+		return location;
+	}
 }
